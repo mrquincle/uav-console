861c306351bf6107a843220bb201e8ea81d77e7f
Anne van Rossum <anne@almende.org>
2013-02-21 18:32:49 +0100

    Version gets info from the drone

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express    = require('express')
   , app        = module.exports = express.createServer()
+  //, net        = require('net')
+  , jot        = require('json-over-tcp')
   ;
+
+var drone = {
+	uav_id: 0,
+	battery_left: 10
+};
+
+var drones = new Array(); 
 
 app.configure(function(){
   app.use(express.bodyParser());
@@ -9,24 +18,67 @@ app.configure(function(){
   app.use(express.static(__dirname + '/libs')); //js files
 });
 
-app.get('/tasks', function (_,response) {
-  response.send(
-    [ { "title": "Wire the money to Panama", "isDone": true }
-    , { "title": "Get hair dye, beard trimmer, dark glasses and \"passport\"", "isDone": false}
-    , { "title": "Book taxi to airport", "isDone": false }
-    , { "title": "Arrange for someone to look after the cat", "isDone": false }
-    ]);
-});
 
-
-app.post('/tasks', function (request,response) {
+app.post('/fire', function (request,response) {
   response.send(request.body); //echo
 });
 
-app.listen(process.argv[2]||8082);
+app.post('/drone/:abort', function (request,response) {
+  response.send(request.body); //echo
+});
+
+/*
+ Provide API call for drone state information 
+*/
+app.get('/drone/:id', function (request,response) {
+	var droneJSON = JSON.stringify(drones[request.params.id]); 
+	response.send(droneJSON); 
+});
+
+app.listen(process.argv[2]||8080);
+
+var cmdPort = 8082;
+
+var server = jot.createServer(cmdPort);
+
+
+server.on('connection', newConnectionHandler);
+
+function droneExists(uav_id) {
+  console.log("Find drone: " + uav_id);
+    for (var i = 0; i < drones.length; i++) {
+    	if (drones[i].uav_id == uav_id) return true;
+    }
+    return false;
+};
+
+// Triggered whenever something connects to the server
+function newConnectionHandler(socket){
+  // Whenever a connection sends us an object...
+  socket.on('data', function(data){
+    // Output the question property of the client's message to the console
+    console.log("UAV state: " + data.state);
+    console.log("Battery level: " + data.battery_left);
+    console.log("Check " + drones.length + " existing drones");
+    if (!droneExists(data.uav_id)) {
+    	//drone.uav_id = data.uav_id;
+    	//drone.battery_left = data.battery_left;
+    	drones.push(data);
+    }
+    // Wait one second, then write an answer to the client's socket
+    setTimeout(function(){
+      socket.write({answer: 42});
+    }, 1000);
+  });
+    
+};
+
+// Start listening
+server.listen(cmdPort);
 
 process.on('uncaughtException', function(err) {
   console.log(JSON.stringify(err));
 });
+
 
 console.log('Start firefox and point it to http://localhost:8082');
--- static/model.js
+++ static/model.js
@@ -1,3 +1,43 @@
+
+function droneparameter(parameter, value) {
+	var self = this;
+	self.parameter = ko.observable(parameter);
+	self.value  = ko.observable(value);
+}
+
+/*
+http://gis.stackexchange.com/questions/5265/using-the-proj4js-library-to-convert-from-google-maps-to-projected-values
+http://www.spatialmarkets.com/blog/2012/2/29/proj4js-and-on-the-fly-coordinate-conversion.html
+*/
+
+//collects the X and the Y in state plane, and returns a point with the lat/lng in it...
+// check http://spatialreference.org/ref/epsg/32633/
+// from http://www.dmap.co.uk/utmworld.htm we are in zone 31U and not 31Q
+function ConvertStatePlane(x,y) {
+	//Proj4js.defs["EPSG:3419"] = "+proj=lcc +lat_1=39.78333333333333 +lat_2=38.71666666666667 +lat_0=38.33333333333334 +lon_0=-98 +x_0=399999.99998984 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs";
+	Proj4js.defs["GOOGLE"]="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs";
+ 	Proj4js.defs["EPSG:4326"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+	Proj4js.defs["EPSG:41001"] = "+title=simple mercator EPSG:41001 +proj=merc +lat_ts=0 +lon_0=0 +k=1.000000 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m";
+	//Proj4js.defs["SR-ORG:111"] = "+proj=utm +zone=31 +ellps=clrk66 +units=m +no_defs";
+
+	//var source = new Proj4js.Proj('EPSG:3419');
+	//var dest = new Proj4js.Proj('EPSG:4326');
+	//var source = new Proj4js.Proj('SR-ORG:111'); // UTM
+	var source = new Proj4js.Proj("EPSG:41001"); // UTM
+	var dest = new Proj4js.Proj('EPSG:4326'); // Google maps
+    //var dest = new Proj4js.Proj("GOOGLE");
+            
+	var point = new Proj4js.Point(x,y);
+	Proj4js.transform(source,dest,point);
+
+	alert("Transformed: " + point.y + "," + point.x);  
+
+	return point;
+}
+
+/*
+ Currently the function name "point" is a misnomer. It should be a drone object
+ */
 function point(name, lat, long) {
     var self = this;
 //    self.id = ko.observable(id);
@@ -28,11 +68,11 @@ function point(name, lat, long) {
     };
 
     self.abortMission = function() {
-	alert("Mission aborted for " + self.name() + ", return to base...");
+		alert("Mission aborted for " + self.name() + ", return to base...");
     };
 
     self.startLanding = function() {
-	alert("Landing started for " + self.name() + "...");
+		alert("Landing started for " + self.name() + "...");
     };
 
     google.maps.event.addListener(marker, 'mouseover', function() {
@@ -42,7 +82,7 @@ function point(name, lat, long) {
     //if you need the poition while dragging
     google.maps.event.addListener(marker, 'drag', function() {
         var pos = marker.getPosition();
-	var decimals = 6;
+		var decimals = 6;
         self.lat(Math.round(pos.lat()*Math.pow(10,decimals))/Math.pow(10,decimals));
         self.long(Math.round(pos.lng()*Math.pow(10,decimals))/Math.pow(10,decimals));
     }.bind(this));
@@ -50,7 +90,7 @@ function point(name, lat, long) {
     //if you just need to update it when the user is done dragging
     google.maps.event.addListener(marker, 'dragend', function() {
         var pos = marker.getPosition();
-	var decimals = 6;
+		var decimals = 6;
         self.lat(Math.round(pos.lat()*Math.pow(10,decimals))/Math.pow(10,decimals));
         self.long(Math.round(pos.lng()*Math.pow(10,decimals))/Math.pow(10,decimals));
     }.bind(this));
@@ -62,7 +102,7 @@ var map = new google.maps.Map(document.getElementById('map'), {
     mapTypeId: google.maps.MapTypeId.TERRAIN
 });
 
-function viewModel() {
+function DroneViewModel() {
     var self = this;
     self.chosenUAVId = ko.observable();
     self.chosenUAV = ko.observable();
@@ -72,6 +112,10 @@ function viewModel() {
         new point('UAV 2', 51.448, 4.344),
         new point('UAV 3', 51.450, 4.338),
         new point('UAV 4', 51.443, 4.337)])
+
+	self.droneparameters = ko.observableArray([
+		new droneparameter('Name','anonymous')
+	])
 
     // mission abort of all selected planes
     self.abortMission = function() {
@@ -88,11 +132,31 @@ function viewModel() {
        self.chosenUAV(uav);
     };
 
-    self.addUAV = function() {
-        self.points.push(new point('UAV X', 51.448, 4.34));
+    self.addUAV = function(name, x, y) {
+    	var coord = ConvertStatePlane(x,y);
+        self.points.push(new point(name, coord.x, coord.y));
     };
 
+	self.loadUAVs = function () {
+		$.getJSON('drone/0', function (dronelist) {
+			self.droneparameters.removeAll();
+			$.each(dronelist, function (field, value) {
+				self.droneparameters.push(new droneparameter(field, value));
+			});
+			self.addUAV('Test', 8900, 10180);
+			//for (var i = 0; i < dronelist.length; i++) {
+				if (droneparameters.uav_id == 0) {
+					self.addUAV(uav_id, uav_x, uav_y);
+				}
+			//}			
+		});
+	};
 };
+	
 
-ko.applyBindings(new viewModel());
 
+$(function () {
+	my = { viewModel: new DroneViewModel() };
+	ko.applyBindings(my.viewModel);
+    my.viewModel.loadUAVs();
+});
